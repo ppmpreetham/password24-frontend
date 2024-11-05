d0d53d0cd206efa9da5cdb7461dca5dc6f85d7f3
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-11-05 11:32:30 +0530

    cam-rotation

--- pwd24/src/components/three.vue
+++ pwd24/src/components/three.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import { TresCanvas } from '@tresjs/core'
 import { Stars } from '@tresjs/cientos'
 import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three'
@@ -11,12 +12,29 @@ const gl = {
   outputColorSpace: SRGBColorSpace,
   toneMapping: NoToneMapping,
 }
+
+const cameraPosition = ref([0, 0, 5])
+
+onMounted(() => {
+  let angle = 0
+  const radius = 5
+  const animate = () => {
+    angle += 0.02
+    cameraPosition.value = [
+      radius * Math.sin(angle),
+      0,
+      radius * Math.cos(angle),
+    ]
+    requestAnimationFrame(animate)
+  }
+  animate()
+})
 </script>
 
 <template>
   <div class="tres-container">
     <TresCanvas v-bind="gl">
-      <TresPerspectiveCamera :position="[0, 0, 5]" />
+      <TresPerspectiveCamera :position="cameraPosition" />
       <Stars />
     </TresCanvas>
   </div>
@@ -24,7 +42,7 @@ const gl = {
 
 <style>
 .tres-container {
-  width: 100vw;
+  width: 100%;
   height: 100vh;
 }
 
