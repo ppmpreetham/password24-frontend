05b2ae762530a5da15980ca54720d5a129242134
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-11-05 10:54:59 +0530

    added stars

--- pwd24/src/components/three.vue
+++ pwd24/src/components/three.vue
@@ -1,6 +1,35 @@
 <script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+import { Stars } from '@tresjs/cientos'
+import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three'
 
+const gl = {
+  clearColor: '#000000',
+  shadows: true,
+  alpha: false,
+  shadowMapType: BasicShadowMap,
+  outputColorSpace: SRGBColorSpace,
+  toneMapping: NoToneMapping,
+}
 </script>
+
 <template>
-  <div>Hello YeS</div>
+  <div class="tres-container">
+    <TresCanvas v-bind="gl">
+      <TresPerspectiveCamera :position="[0, 0, 5]" />
+      <Stars />
+    </TresCanvas>
+  </div>
 </template>
+
+<style>
+.tres-container {
+  width: 100vw;
+  height: 100vh;
+}
+
+canvas {
+  width: 100%;
+  height: 100%;
+}
+</style>
