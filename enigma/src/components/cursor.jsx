b0994df1721aaf5af45901ea3f613b010e914efd
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-11-29 18:45:33 +0530

    fix: hovering links bug

--- enigma/src/components/cursor.jsx
+++ enigma/src/components/cursor.jsx
@@ -22,7 +22,7 @@ function MovableSquare() {
       {positions.map((position, index) => (
         <div
           key={index}
-          className='fixed w-3 h-3 bg-enigma-green'
+          className='fixed w-3 h-3 bg-enigma-green pointer-events-none'
           style={{
             top: position.y,
             left: position.x,
