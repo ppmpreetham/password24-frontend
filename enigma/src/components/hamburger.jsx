0a7663391a0694468078914d6fad0fff8ab29bf4
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-11-29 20:35:32 +0530

    added the hamburger

--- enigma/src/components/hamburger.jsx
+++ enigma/src/components/hamburger.jsx
@@ -1,8 +1,29 @@
+import React, { useState } from 'react';
+
 export default function Hamburger() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <div>
-            <span className="text-3xl font-montreal-medium">This is a </span>
-            <span className="font-mondwest text-4xl">Hamburger</span>
+        <div className="flex justify-end">
+            <div className={`flex flex-col justify-end text-black bg-enigma-green font-neuebit uppercase ${isOpen ? 'w-1/5' : 'w-auto'}`}>
+                <div className="hamburger-icon cursor-pointer text-2xl select-none text-right p-4" onClick={toggleMenu}>
+                    Menu <span className="text-xl">&#9776;</span> {/* Hamburger icon :D */}
+                </div>
+                {isOpen && (
+                    <div className="menu text-right text-3xl p-4">
+                        <ul>
+                            <li><a className="text-black hover:bg-white hover:text-black" href="#home">Home</a></li>
+                            <li><a className="text-black hover:bg-white hover:text-black" href="#about">About</a></li>
+                            <li><a className="text-black hover:bg-white hover:text-black" href="#services">Services</a></li>
+                            <li><a className="text-black hover:bg-white hover:text-black" href="#contact">Contact</a></li>
+                        </ul>
+                    </div>
+                )}
+            </div>
         </div>
-    )
+    );
 }
