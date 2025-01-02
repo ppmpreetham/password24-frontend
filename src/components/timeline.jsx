27ed6bca0fa860d59587bcf99991cbd9af241d93
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-01-02 12:34:01 +0530

    movable timeline component

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,8 +1,9 @@
 import React, { useRef, useEffect } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { Plane, OrbitControls } from '@react-three/drei';
+import { Plane, OrbitControls, ScrollControls, useScroll } from '@react-three/drei';
 import { createNoise2D } from 'simplex-noise';
 import * as THREE from 'three';
+import { gsap } from 'gsap';
 
 const noise2D = createNoise2D();
 const height = 0.1;
@@ -54,9 +55,33 @@ function Scene() {
   const { scene, gl } = useThree();
 
   useEffect(() => {
-    scene.fog = new THREE.FogExp2('black', 0.015);
-    gl.setClearColor('black');
+    scene.fog = new THREE.FogExp2('#111111', 0.02);
+    // gl.setClearColor('#111111');
   }, [scene, gl]);
+
+  return null;
+}
+
+function CameraAnimation() {
+  const { camera } = useThree();
+  const scroll = useScroll();
+  const curve = new THREE.CatmullRomCurve3([
+    new THREE.Vector3(0, 5, 0),
+    new THREE.Vector3(1, 5, -10),
+    new THREE.Vector3(20, 5, -20),
+    new THREE.Vector3(30, 5, -30),
+    new THREE.Vector3(40, 5, -40),
+    new THREE.Vector3(50, 5, -40),
+  ]);
+
+  useFrame(() => {
+    const scrollOffset = scroll.offset;
+    const point = curve.getPointAt(scrollOffset);
+    const tangent = curve.getTangentAt(scrollOffset);
+
+    camera.position.copy(point);
+    camera.lookAt(point.clone().add(tangent));
+  });
 
   return null;
 }
@@ -66,16 +91,19 @@ function Timeline() {
     <div className="h-screen">
       <Canvas
         camera={{
-          position: [0.2166624773016082, 7.317632121626254, 29.524806868365605],
+          position: [0.2, 7.5, 30],
           fov: 45,
-          rotation: [-0.24295120019828584, 0.007122689397216926, 0.0017653197905153335]
+          rotation: [-0,25, 0, 0]
         }}
+
         className="w-full h-full"
         gl={{ alpha: false, antialias: true }}
       >
-        <Scene />
-        <AnimatedPlane />
-        <CameraControls />
+        <ScrollControls pages={6} damping={1}>
+          <CameraAnimation />
+          <Scene />
+          <AnimatedPlane />
+        </ScrollControls>
       </Canvas>
     </div>
   );
