93112ee9d62ddb0f3a85407016cb9ebd86a9f830
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-01-04 03:36:07 +0530

    remove: privacy page

--- src/assets/ertdfgcvb/programsJS/src/modules/camera.js
+++ src/assets/ertdfgcvb/programsJS/src/modules/camera.js
@@ -9,47 +9,47 @@ returns a video element (initialised asynchronously).
 
 export default { init }
 
-let video
-function init(callback) {
-	// Avoid double init of video object
-	video = video || getUserMedia(callback)
-	return video
+let video;
+async function init(callback) {
+    // Avoid double init of video object
+    if (!video) {
+        video = await getUserMedia(callback);
+    }
+    return video;
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-function getUserMedia(callback) {
+async function getUserMedia(callback) {
+    // getUserMedia is not supported by browser
+    if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
+        throw new DOMException('getUserMedia not supported in this browser');
+    }
 
-	// getUserMedia is not supported by browser
-	if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
-		throw new DOMException('getUserMedia not supported in this browser')
-		return
-	}
+    // Create a video element
+    const video = document.createElement('video');
+    video.setAttribute('playsinline', ''); // Required to work in iOS 11 & up
 
-	// Create a video element
-	const video = document.createElement('video')
-	video.setAttribute('playsinline', '') // Required to work in iOS 11 & up
+    const constraints = {
+        audio: false,
+        video: { facingMode: "user" }
+    };
 
-	const constraints = {
-		audio: false,
-		video: { facingMode: "user" }
-	}
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        if ('srcObject' in video) {
+            video.srcObject = stream;
+        } else {
+            video.src = window.URL.createObjectURL(stream);
+        }
+        video.addEventListener('loadedmetadata', function() {
+            video.play();
+            if (typeof callback === 'function') callback(video.srcObject);
+        });
+        console.log('Camera permission granted');
+    } catch (error) {
+        console.error('Camera permission denied', error);
+        throw error;
+    }
 
-	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-		if ('srcObject' in video) {
-			video.srcObject = stream
-		} else {
-			video.src = window.URL.createObjectURL(stream)
-		}
-	}).catch(function(err) {
-		let msg = 'No camera available.'
-		if (err.code == 1) msg = 'User denied access to use camera.'
-		console.log(msg);
-		console.error(err)
-	})
-
-	video.addEventListener('loadedmetadata', function() {
-		video.play()
-		if (typeof callback === 'function') callback(video.srcObject)
-	})
-	return video
+    return video;
 }
