fafd9eba6254353e73bd8fec4f8851a7b76aae0d
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-01-04 03:20:37 +0530

    try fix: deployment

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import PageNotFound from "./404";
 import PageTransition from "./components/PageTransition";
 import { gsap } from "gsap";
 import './App.css';
-import Privacy from "./privacy";
+import ErtdfgcvbBG from "./components/ertdfgcvbprivacy";
 
 function App() {
     const location = useLocation();
@@ -43,7 +43,7 @@ function App() {
                         <Route exact path="/" element={<Home />} />
                         <Route path="/about" element={<About />} />
                         <Route path="/sign-up" element={<SignUp />} />
-                        <Route path="/privacy" element={<Privacy />} />
+                        <Route path="/privacy" element={<ErtdfgcvbBG />} />
                         <Route path="*" element={<PageNotFound />} />
                     </Routes>
                 </CSSTransition>
