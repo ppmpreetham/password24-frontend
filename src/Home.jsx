3506b7459f3cf0774d3ea4b6e1a26c6385538d26
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-01-04 00:42:13 +0530

    added timeline text

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,10 +15,11 @@ function Home() {
       {/* <MovableSquare /> */}
         <Join className='z-20'/>
         <nav className="sticky top-0 flex justify-between w-full px-10 py-5 z-20">
-          <div className='text-5xl font-calcio text-left pt-3'>ENIGMA</div>
+          <div className='text-5xl font-neuebit text-left pt-3'>ENIGMA</div>
           <Hamburger className='fixed right-0'/>
         </nav>
         <ErtdfgcvbBG className='z-0'/>
+        <h2 className="font-neuebit uppercase text-9xl">TIMELINE</h2>
         <Timeline className='w-full z-10'/>
         <Footer />
         {/* <BackgroundEffect /> */}
