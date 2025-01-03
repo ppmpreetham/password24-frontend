831741b4326496845348d579bbc5d6b41d1356cc
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-01-04 01:33:20 +0530

    update: better routing

--- src/components/join.jsx
+++ src/components/join.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import AnimatedText from './text';
 
 export default function Join({ className }) {
-    const navigate = useNavigate();
-
-    const handleClick = () => {
-        navigate('/sign-up');
-    };
-
     return (
         <div className={className}>
-            <button 
-                className="fixed bottom-0 right-0 rounded-none text-right m-6 bg-enigma-green px-6 py-4 text-black font-neuebit text-6xl flex items-center justify-center h-24"
-                onClick={handleClick}
-            >
-                JOIN
-            </button>
+            <Link to="/sign-up">
+                <AnimatedText text="JOIN" className="fixed bottom-0 right-0 rounded-none text-right m-6 bg-enigma-green px-6 py-4 text-black font-neuebit text-6xl flex items-center justify-center h-24"/>
+            </Link>
         </div>
     );
 }
